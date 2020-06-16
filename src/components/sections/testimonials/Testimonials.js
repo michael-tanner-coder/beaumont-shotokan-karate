2e4162a7b40d601334dc944dd92d79084d293b15
michael-tanner-coder <michaelmontytanner@gmail.com>
2020-06-16 17:50:31 -0500

    Added testimonial content

--- src/components/sections/testimonials/Testimonials.js
+++ src/components/sections/testimonials/Testimonials.js
@@ -14,20 +14,20 @@ const Testimonials = (props, { style: Style = StyledSection }) => {
           <Row xs={1} md={1} lg={3}>
             <Col>
               <Quote
-                text="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-                citation="Person"
+                text="BSK helped me grow as a person. I'm more confident than ever!"
+                citation="Zachary, green belt"
               ></Quote>
             </Col>
             <Col>
               <Quote
-                text="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-                citation="Person"
+                text="I learned so much and made so many friends!"
+                citation="Eliza, brown belt"
               ></Quote>
             </Col>
             <Col>
               <Quote
-                text="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-                citation="Person"
+                text="Our sensei trains us hard, but it's always worth the effort!"
+                citation="Rajat, purple belt"
               ></Quote>
             </Col>
           </Row>
