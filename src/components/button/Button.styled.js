751ca49c3b6683d70f05e993e942e7dff9be934a
michael-tanner-coder <michaelmontytanner@gmail.com>
2020-06-18 15:44:11 -0500

    Fixing button appearance on safari

--- src/components/button/Button.styled.js
+++ src/components/button/Button.styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 const StyledButton = styled.div`
   .btn {
+    -webkit-appearance: none;
     background-color: ${({ theme }) => theme.primary_color};
     color: ${({ theme }) => theme.secondary_color};
     border-color: ${({ theme }) => theme.primary_color};
