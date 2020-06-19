aaf4a2b008449b263c9a50922e0ef2dc321f6568
michael-tanner-coder <michaelmontytanner@gmail.com>
2020-06-19 10:24:53 -0500

    Custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,38 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Container, Jumbotron } from "react-bootstrap"
+import Heading from "../components/text/Heading"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <br />
+      <Jumbotron>
+        <h1>404: PAGE NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist.</p>
+        <p>Were you looking for one of these pages?</p>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/classes">Classes</Link>
+          </li>
+          <li>
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/faq">FAQ</Link>
+          </li>
+        </ul>
+      </Jumbotron>
+    </Container>
   </Layout>
 )
 
