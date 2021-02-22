c49dfeba21c9d334db5897dbf25531ef6ba1460d
michael-tanner-coder <michaelmontytanner@gmail.com>
2021-02-21 22:47:13 -0600

    Themes moved to separate folder

---
+++ src/themes/theme_night.js
@@ -0,0 +1,10 @@
+const theme_alt = {
+  primary_color: "#4074a7",
+  secondary_color: "#f5f5f5",
+  select_color: "#e59263",
+  cta_color: "#de6f40",
+  primary_font_color: "#f5f5f5",
+  background_color: "#363C44",
+}
+
+export default theme_alt
